95068a79ca9ae3d4ab3593a58a7e3d49f73f4263
Hoplite2A <stgdave.mailloux#gmail.com>
2023-11-16 20:04:06 -0500

    Workshop Completed / Final Block028 Commit

--- workshop/src/App.jsx
+++ workshop/src/App.jsx
@@ -1,26 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
+import "./App.css";
 
-function App() {
-
+export default function App() {
   return (
     <>
       <div id="container">
-        <h1>
-
-        </h1>
+        <h1></h1>
         <div id="navbar">
-
+          <Link to="/home">Home</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
         </div>
         <div id="main-section">
           <Routes>
-              <Route path="/blue" element={<h1>Blue</h1>} />
-              <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
           </Routes>
         </div>
       </div>
     </>
-  )
+  );
 }
-
-export default App
