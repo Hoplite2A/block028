0c99e9181753c5cc5f37d2372baa68c93caa9cbe
Hoplite2A <stgdave.mailloux#gmail.com>
2023-11-16 19:42:00 -0500

    Intializing Commit with Workshop's Base Code

---
+++ workshop/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="container">
+        <h1>
+
+        </h1>
+        <div id="navbar">
+
+        </div>
+        <div id="main-section">
+          <Routes>
+              <Route path="/blue" element={<h1>Blue</h1>} />
+              <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
